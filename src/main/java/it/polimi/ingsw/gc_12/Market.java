33c6bb3f1a2f74e91c8df14873697d4ddf26654c
ruggero95 <10454587@polimi.it>
2017-05-13 14:34:48 +0200

    Implementation of Market

---
+++ src/main/java/it/polimi/ingsw/gc_12/Market.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.gc_12;
+
+import java.util.List;
+
+public class Market {
+	private final int numberOfSpaces;
+	private List<SpaceMarket> spaceMarket;
+	
+	public Market(int setNumber){
+		if(setNumber<0){
+			throw new IllegalArgumentException();
+		}else{
+			this.numberOfSpaces=setNumber;
+		}
+		for(int i=0;i<numberOfSpaces;i++){
+			/*TODO implements xml configuration with the right spacemarket constructor*/
+			spaceMarket.add(new SpaceMarket());
+		}
+	}
+	public int getNumberOfSpaces(){
+		return numberOfSpaces;
+	}
+}
