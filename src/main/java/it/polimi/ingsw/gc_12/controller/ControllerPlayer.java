d42a2ebbcc21019708a09bf8643eba2235a0c653
Federico Sandrelli <federico.sandrelli@gmail.com>
2017-05-16 16:36:34 +0200

    Empty ControllerPlayer created, waiting for view implementation

---
+++ src/main/java/it/polimi/ingsw/gc_12/controller/ControllerPlayer.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.gc_12.controller;
+
+import it.polimi.ingsw.gc_12.Player;
+
+public class ControllerPlayer {
+	private final Player player;
+	
+	public ControllerPlayer(Player player){
+		this.player = player;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public void handleAction(){
+		
+	}
+	
+	public void handleVaticanReport(int period){
+		
+	}
+	
+	private boolean askExcommuication(){
+		return false;
+	}
+	
+	private boolean askActionType(){
+		return false;
+		
+	}
+	private boolean askFamilyMember(){
+		return false;
+		
+	}
+	private boolean askOccupiable(){
+		return false;
+	
+	}
+	private boolean askCardLeader(){
+		return false;
+		
+	}
+	private boolean askExcommunication(){
+		return false;
+		
+	}
+	
+}
