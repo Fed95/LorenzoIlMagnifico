15878600e6b6e0d3766ead2327581adf34f0c62b
Marco Varrone <marco.varrone@mail.polimi.it>
2017-05-23 14:31:02 +0200

    Card requirements as ArrayList of Resources instead of array

--- src/main/java/it/polimi/ingsw/gc_12/card/CardDevelopment.java
+++ src/main/java/it/polimi/ingsw/gc_12/card/CardDevelopment.java
@@ -9,7 +9,7 @@ public abstract class CardDevelopment extends Card{
 	private final CardType cardType;
 	private final int period;
 	
-	public CardDevelopment(int id, CardType cardType, String name, int period, Resource[] requirements, List<Effect> effects) {
+	public CardDevelopment(int id, CardType cardType, String name, int period, List<Resource> requirements, List<Effect> effects) {
 		super(id, name, requirements, effects);
 		this.cardType = cardType;
 		this.period = period;
