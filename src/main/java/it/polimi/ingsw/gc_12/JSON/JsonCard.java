b8f8efe04a3554e2f5a8e4c91d929bd7ce2fef33
ruggero95 <10454587@polimi.it>
2017-05-25 17:56:46 +0200

    Added all runtime type

--- src/main/java/it/polimi/ingsw/gc_12/JSON/JsonCard.java
+++ src/main/java/it/polimi/ingsw/gc_12/JSON/JsonCard.java
@@ -8,8 +8,11 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 
-import it.polimi.ingsw.gc_12.card.Card;
+import it.polimi.ingsw.gc_12.*;
+import it.polimi.ingsw.gc_12.card.*;
 import it.polimi.ingsw.gc_12.effect.*;
+import it.polimi.ingsw.gc_12.event.Event;
+import it.polimi.ingsw.gc_12.event.EventPlaceFamilyMember;
 import it.polimi.ingsw.gc_12.resource.Resource;
 import it.polimi.ingsw.gc_12.resource.*;
 
@@ -19,7 +22,7 @@ public class JsonCard extends JsonMaster {
 	}
 	public void createCards(List<Card> cards){
 		Gson gsonobj=new Gson();
-		String gsoncard=gsonobj.toJson(cards);
+		String gsoncard=gsonobj.toJson(cards);		
 		ManageJsonFile manageJsonFile=new ManageJsonFile();
 		manageJsonFile.tojsonFile(gsoncard, this);
 	}
@@ -31,17 +34,42 @@ public class JsonCard extends JsonMaster {
 		String json=manageJsonFile.fromJsonFile(this);
 		//GsonBuilder gsonBldr = new GsonBuilder();
 		//gsonBldr.registerTypeAdapter(listCardType,new CardPersonalDeserializer());
+		final RuntimeTypeAdapterFactory<EffectProvider> typeFactory6 = RuntimeTypeAdapterFactory  
+		        .of(EffectProvider.class, "listEffectProvider")
+		        .registerSubtype(Card.class, "CARD")
+		        .registerSubtype(Occupiable.class, "OCCUPIABLE");
 		
-		final RuntimeTypeAdapterFactory<Resource> typeFactory = RuntimeTypeAdapterFactory  
-		        .of(Resource.class, "type") // Here you specify which is the parent class and what field particularizes the child class.
-		        .registerSubtype(Wood.class, "WOOD") // if the flag equals the class name, you can skip the second parameter. This is only necessary, when the "type" field does not equal the class name.
+		final RuntimeTypeAdapterFactory<Occupiable> typeFactory5 = RuntimeTypeAdapterFactory  
+		        .of(Occupiable.class, "listOccupiable")
+		        .registerSubtype(SpaceMarket.class, "SPACE_MARKET")
+		        .registerSubtype(TowerFloor.class, "TOWER_FLOOR")
+		        .registerSubtype(SpaceWork.class, "SPACE_WORK");//council palace
+		
+		final RuntimeTypeAdapterFactory<Card> typeFactory = RuntimeTypeAdapterFactory  
+		        .of(Card.class, "type")
+		        .registerSubtype(CardDevelopment.class, "DEVELOPMENT");
+		        //registerSubtype(CardLeader.class, "LEADER");
+		
+		final RuntimeTypeAdapterFactory<CardDevelopment> typeFactory1 = RuntimeTypeAdapterFactory  
+		        .of(CardDevelopment.class, "cardDevelopmentType")
+		        .registerSubtype(CardBuilding.class, "BUILDING");
+		
+		final RuntimeTypeAdapterFactory<Resource> typeFactory2 = RuntimeTypeAdapterFactory  
+		        .of(Resource.class, "type") 
+		        .registerSubtype(Wood.class, "WOOD") 
 		        .registerSubtype(Stone.class, "STONE");
-		final RuntimeTypeAdapterFactory<Effect> typeFactory2 = RuntimeTypeAdapterFactory  
+		final RuntimeTypeAdapterFactory<Effect> typeFactory3 = RuntimeTypeAdapterFactory  
 		        .of(Effect.class, "type") // Here you specify which is the parent class and what field particularizes the child class.
 		        .registerSubtype(EffectChangeResource.class, "EFFECT_CHANGE_RESOURCE")
 		        .registerSubtype(EffectChangeFamilyMemberValue.class, "EFFECT_CHANGE_FAMILY_MEMBER_VALUE"); // if the flag equals the class name, you can skip the second parameter. This is only necessary, when the "type" field does not equal the class name.
-		    
-		Gson gson = new GsonBuilder().registerTypeAdapterFactory(typeFactory).registerTypeAdapterFactory(typeFactory2).create();
+		final RuntimeTypeAdapterFactory<Event> typeFactory4 = RuntimeTypeAdapterFactory  
+		        .of(Event.class, "eventType")
+		        .registerSubtype(EventPlaceFamilyMember.class, "EVENT_PLACE_FAMILY_MEMBER");
+		
+	
+		
+		
+		Gson gson = new GsonBuilder().registerTypeAdapterFactory(typeFactory6).registerTypeAdapterFactory(typeFactory5).registerTypeAdapterFactory(typeFactory4).registerTypeAdapterFactory(typeFactory3).registerTypeAdapterFactory(typeFactory2).registerTypeAdapterFactory(typeFactory1).registerTypeAdapterFactory(typeFactory).create();
 		
 		List<Card> cards=gson.fromJson(json, listCardType);
 
