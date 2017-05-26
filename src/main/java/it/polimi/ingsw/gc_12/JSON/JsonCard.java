ce51ad512e943094b4eb3dbc899b28689e19368b
Marco Varrone <marco.varrone@mail.polimi.it>
2017-05-26 19:22:44 +0200

    Add some types to the RuntimeTypeAdapterFactories in JsonCard

--- src/main/java/it/polimi/ingsw/gc_12/JSON/JsonCard.java
+++ src/main/java/it/polimi/ingsw/gc_12/JSON/JsonCard.java
@@ -8,13 +8,11 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 
-import it.polimi.ingsw.gc_12.card.Card;
+import it.polimi.ingsw.gc_12.card.*;
 import it.polimi.ingsw.gc_12.*;
-import it.polimi.ingsw.gc_12.card.CardBuilding;
 import it.polimi.ingsw.gc_12.effect.*;
 import it.polimi.ingsw.gc_12.event.Event;
 import it.polimi.ingsw.gc_12.event.EventPlaceFamilyMember;
-import it.polimi.ingsw.gc_12.resource.Resource;
 import it.polimi.ingsw.gc_12.resource.*;
 
 public class JsonCard extends JsonMaster {
@@ -28,33 +26,41 @@ public class JsonCard extends JsonMaster {
 	private Gson buildGson() {
 		final RuntimeTypeAdapterFactory<EffectProvider> factoryEffectProvider = RuntimeTypeAdapterFactory
 				.of(EffectProvider.class, "effectProvider")
-				.registerSubtype(Card.class, Card.class.getName())
-				.registerSubtype(Occupiable.class, Occupiable.class.getName());
+				.registerSubtype(Card.class, Card.class.getSimpleName())
+				.registerSubtype(Occupiable.class, Occupiable.class.getSimpleName());
 
 		final RuntimeTypeAdapterFactory<Occupiable> factoryOccupiable = RuntimeTypeAdapterFactory
 				.of(Occupiable.class, "occupiable")
-				.registerSubtype(SpaceMarket.class, SpaceMarket.class.getName())
-				.registerSubtype(TowerFloor.class, TowerFloor.class.getName())
-				.registerSubtype(SpaceWork.class, SpaceWork.class.getName());//council palace
+				.registerSubtype(SpaceMarket.class, SpaceMarket.class.getSimpleName())
+				.registerSubtype(TowerFloor.class, TowerFloor.class.getSimpleName())
+				.registerSubtype(SpaceWork.class, SpaceWork.class.getSimpleName());//council palace
 
 		final RuntimeTypeAdapterFactory<Card> factoryCard = RuntimeTypeAdapterFactory
 				.of(Card.class, "type")
-				.registerSubtype(CardBuilding.class, CardBuilding.class.getName());
+				.registerSubtype(CardBuilding.class, CardBuilding.class.getSimpleName())
+				.registerSubtype(CardCharacter.class, CardCharacter.class.getSimpleName())
+				.registerSubtype(CardTerritory.class, CardTerritory.class.getSimpleName())
+				.registerSubtype(CardVenture.class, CardVenture.class.getSimpleName());
 				//registerSubtype(CardLeader.class, "LEADER");
 
 		final RuntimeTypeAdapterFactory<Resource> factoryResource = RuntimeTypeAdapterFactory
-				.of(Resource.class, "type")
-				.registerSubtype(Wood.class, Wood.class.getName())
-				.registerSubtype(Stone.class, Stone.class.getName());
+				.of(Resource.class, "resourceType")
+				.registerSubtype(FaithPoint.class, FaithPoint.class.getSimpleName())
+				.registerSubtype(MilitaryPoint.class, MilitaryPoint.class.getSimpleName())
+				.registerSubtype(Money.class, Money.class.getSimpleName())
+				.registerSubtype(Servant.class, Servant.class.getSimpleName())
+				.registerSubtype(Stone.class, Stone.class.getSimpleName())
+				.registerSubtype(VictoryPoint.class, VictoryPoint.class.getSimpleName())
+				.registerSubtype(Wood.class, Wood.class.getSimpleName());
 
 		final RuntimeTypeAdapterFactory<Effect> factoryEffect = RuntimeTypeAdapterFactory
 				.of(Effect.class, "type") // Here you specify which is the parent class and what field particularizes the child class.
-				.registerSubtype(EffectChangeResource.class, EffectChangeResource.class.getName())
-				.registerSubtype(EffectChangeFamilyMemberValue.class, EffectChangeFamilyMemberValue.class.getName()); // if the flag equals the class name, you can skip the second parameter. This is only necessary, when the "type" field does not equal the class name.
+				.registerSubtype(EffectChangeResource.class, EffectChangeResource.class.getSimpleName())
+				.registerSubtype(EffectChangeFamilyMemberValue.class, EffectChangeFamilyMemberValue.class.getSimpleName()); // if the flag equals the class name, you can skip the second parameter. This is only necessary, when the "type" field does not equal the class name.
 
 		final RuntimeTypeAdapterFactory<Event> factoryEvent = RuntimeTypeAdapterFactory
 				.of(Event.class, "eventType")
-				.registerSubtype(EventPlaceFamilyMember.class, EventPlaceFamilyMember.class.getName());
+				.registerSubtype(EventPlaceFamilyMember.class, EventPlaceFamilyMember.class.getSimpleName());
 
 		return new GsonBuilder()
 				.registerTypeAdapterFactory(factoryEffectProvider)
