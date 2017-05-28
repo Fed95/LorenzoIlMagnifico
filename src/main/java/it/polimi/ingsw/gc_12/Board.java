ff14864849eec7c4368159a04d1d4696e4172b87
Marco Varrone <marco.varrone@mail.polimi.it>
2017-05-28 16:26:05 +0200

    Revert board changes pushed by mistake

--- src/main/java/it/polimi/ingsw/gc_12/Board.java
+++ src/main/java/it/polimi/ingsw/gc_12/Board.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.gc_12;
 
 import it.polimi.ingsw.gc_12.JSON.loader.LoaderMarket;
 import it.polimi.ingsw.gc_12.JSON.loader.LoaderTowerSet;
+import it.polimi.ingsw.gc_12.occupiables.*;
 import it.polimi.ingsw.gc_12.track.TrackTurnOrder;
 
 import java.util.ArrayList;
@@ -12,22 +13,23 @@ public class Board {
 	private SpaceDie spaceDie;
 	private TowerSet towerSet;
 	private Market market;
+	private CouncilPalace councilPalace;
 	private List<Occupiable> spaceWorks = new ArrayList<>();
 	private TrackTurnOrder trackTurnOrder;
-	
+
 	/*
-	private CouncilPalace councilPalace;
 	private ExcommunicationSpace excommunicationSpace;
 	private FaithPointsTrack faithPointTrack;
 	*/
-	
+
 	public Board() {
 		this.spaceDie = SpaceDie.instance();
 		this.towerSet = new LoaderTowerSet().get();
 		this.market = new LoaderMarket().get();
-		this.trackTurnOrder = new TrackTurnOrder(Match.instance().getPlayers());
-		
-		
+		this.councilPalace = new CouncilPalace(1, null); //TODO: import values and effects from Json
+		this.trackTurnOrder = new TrackTurnOrder(Match.instance().getPlayers(), councilPalace);
+
+
 		for(WorkType workType : WorkType.values()){
 			//TODO: set import of requiredValues and effects from Json file
 			SpaceWorkZone spaceWorkZone = new SpaceWorkZone();
@@ -36,24 +38,31 @@ public class Board {
 			spaceWorks.add(spaceWorkSingle);
 			spaceWorks.add(spaceWorkMultiple);
 		}
-		
-		
 	}
-	
+
+	public void refresh(){
+		towerSet.refresh();
+		trackTurnOrder.newRound();
+	}
+
 	public List<Occupiable> getSpaceWorks(){
 		return spaceWorks;
 	}
-	
+
 	public SpaceDie getSpaceDie() {
 		return spaceDie;
 	}
-	
+
 	public TowerSet getTowerSet() {
 		return towerSet;
 	}
-	
+
 	public Market getMarket() {
 		return market;
+	}
+
+	public CouncilPalace getCouncilPalace() {
+		return councilPalace;
 	}
 
 	public TrackTurnOrder getTrackTurnOrder() {
@@ -66,11 +75,8 @@ public class Board {
 		occupiables.addAll(spaceWorks);
 		return occupiables;
 	}
-	
+
 	/*
-	public CouncilPalace getCouncilPalace() {
-		return councilPalace;
-	}
 	public ExcommunicationSpace getExcommunicationSpace() {
 		return excommunicationSpace;
 	}
