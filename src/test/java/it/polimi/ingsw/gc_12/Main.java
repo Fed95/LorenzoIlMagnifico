89c889f59e04f01e27722234df40a22fa57d23fc
Marco Varrone <marco.varrone@mail.polimi.it>
2017-05-28 16:54:01 +0200

    Empty main

--- src/test/java/it/polimi/ingsw/gc_12/Main.java
+++ src/test/java/it/polimi/ingsw/gc_12/Main.java
@@ -1,27 +1,9 @@
 package it.polimi.ingsw.gc_12;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import it.polimi.ingsw.gc_12.JSON.AskCards;
-import it.polimi.ingsw.gc_12.JSON.LoaderCard;
-
-
-import it.polimi.ingsw.gc_12.JSON.LoaderTowerSet;
-import it.polimi.ingsw.gc_12.card.Card;
-
 public class Main {
 
 	public static void main(String[] args) {
-		/*List<Card> cards= new ArrayList<>();
-		LoaderCard jsonobj= new LoaderCard("card");
-		AskCards askCards=new AskCards();
-		cards=askCards.buildCards();
-		jsonobj.createCards(cards);
-		cards=jsonobj.getCards();*/
 
-		LoaderTowerSet loaderTowerSet = new LoaderTowerSet("towerSet");
-		loaderTowerSet.create();
 	}
 
 }
