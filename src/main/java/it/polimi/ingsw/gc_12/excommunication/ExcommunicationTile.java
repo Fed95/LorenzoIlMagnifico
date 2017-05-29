2604435ad1e97fafc156e88b0c3b8a9e65443ba3
ruggero95 <10454587@polimi.it>
2017-05-29 18:50:25 +0200

    Refactor: rename class

--- src/main/java/it/polimi/ingsw/gc_12/excommunication/ExcommunicationTile.java
+++ src/main/java/it/polimi/ingsw/gc_12/excommunication/ExcommunicationTile.java
@@ -5,12 +5,12 @@ import java.util.List;
 import it.polimi.ingsw.gc_12.EffectProvider;
 import it.polimi.ingsw.gc_12.effect.Effect;
 
-public class ExcommunucationTile implements EffectProvider {
+public class ExcommunicationTile implements EffectProvider {
 	private int period;
 	List<Effect> effects;
-	public ExcommunucationTile(int period, List<Effect> effects){
-		this.period=period;
-		this.effects=effects;
+	public ExcommunicationTile(int period, List<Effect> effects){
+		this.period = period;
+		this.effects = effects;
 	}
 	public int getPeriod() {
 		return period;
