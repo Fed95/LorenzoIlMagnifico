33ad396cdc4c40a7584ae81ef949f4cad1c74243
Marco Varrone <marco.varrone@mail.polimi.it>
2017-05-30 09:46:50 +0200

    Temporary empty TestMatch

--- src/test/java/it/polimi/ingsw/gc_12/TestMatch.java
+++ src/test/java/it/polimi/ingsw/gc_12/TestMatch.java
@@ -5,8 +5,7 @@ import it.polimi.ingsw.gc_12.exceptions.NotEnoughResourcesException;
 public class TestMatch {
 
 	public static void main(String[] args){
-		Match.instance().setup();
-		Match.instance().start();
+
 	}
 
 }
