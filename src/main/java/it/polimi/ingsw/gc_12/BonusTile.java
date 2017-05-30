56b4f7daa0d012a4163f9aa620e0cc9a8b2e9e12
ruggero95 <10454587@polimi.it>
2017-05-30 15:12:11 +0200

    Basic implementation

--- src/main/java/it/polimi/ingsw/gc_12/BonusTile.java
+++ src/main/java/it/polimi/ingsw/gc_12/BonusTile.java
@@ -1,5 +1,19 @@
 package it.polimi.ingsw.gc_12;
 
-public class BonusTile {
+import java.util.List;
 
+import it.polimi.ingsw.gc_12.effect.Effect;
+
+public class BonusTile implements EffectProvider {
+	GameMode gameMode;
+	List<Effect> effects;
+	
+	public BonusTile(GameMode gameMode,List<Effect> effects){
+		this.gameMode=gameMode;
+		this.effects=effects;
+	}
+	@Override
+	public List<Effect> getEffects(){
+		return effects;
+	}
 }
