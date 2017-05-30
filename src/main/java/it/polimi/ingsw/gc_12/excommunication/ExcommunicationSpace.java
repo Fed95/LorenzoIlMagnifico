af22481112d8744a1c6fc8abae0ac6fc7c317196
ruggero95 <10454587@polimi.it>
2017-05-30 11:43:52 +0200

    Changed constructor and method chooseTile

--- src/main/java/it/polimi/ingsw/gc_12/excommunication/ExcommunicationSpace.java
+++ src/main/java/it/polimi/ingsw/gc_12/excommunication/ExcommunicationSpace.java
@@ -5,27 +5,28 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import it.polimi.ingsw.gc_12.Match;
+
 public class ExcommunicationSpace {
 	private int excommunicationTileNum;
 	private List<ExcommunicationTile> excommunicationTilesGeneral;
 	private List<ExcommunicationTile> finalExcommunicationTiles;
 
 	
-	public ExcommunicationSpace(int excommunicationTileNum){
+	public ExcommunicationSpace(int excommunicationTileNum, List<ExcommunicationTile> excommunicationTiles){
 		this.excommunicationTileNum = excommunicationTileNum;
+		this.excommunicationTilesGeneral = excommunicationTiles;
 		chooseTiles();
 	}
 	private void chooseTiles() {
-		excommunicationTilesGeneral=new ArrayList<>();//import from json here
-		List<ExcommunicationTile> firstRoundTile = excommunicationTilesGeneral.stream().filter(Tile->Tile.getPeriod()==1).collect(Collectors.toList());
-		List<ExcommunicationTile> secondRoundTile = excommunicationTilesGeneral.stream().filter(Tile->Tile.getPeriod()==2).collect(Collectors.toList());
-		List<ExcommunicationTile> thirdRoundTile =excommunicationTilesGeneral.stream().filter(Tile->Tile.getPeriod()==3).collect(Collectors.toList());
-		Collections.shuffle(firstRoundTile);
-		Collections.shuffle(secondRoundTile);
-		Collections.shuffle(thirdRoundTile);
-		finalExcommunicationTiles.add(firstRoundTile.get(0));
-		finalExcommunicationTiles.add(secondRoundTile.get(0));
-		finalExcommunicationTiles.add(thirdRoundTile.get(0));
-
+		Match match = Match.instance();
+		int periodNum=match.getTotalPeriodNumber();
+		List<ExcommunicationTile> roundTile;
+		for(int i = 0 ; i < periodNum; i++ ){
+			int period = i + 1;
+			roundTile = excommunicationTilesGeneral.stream().filter(Tile->Tile.getPeriod()== period).collect(Collectors.toList());
+			Collections.shuffle(roundTile);
+			finalExcommunicationTiles.add(roundTile.get(0));
+		}
 	}
 }
