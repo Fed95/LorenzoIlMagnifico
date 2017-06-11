9ca0fd64be98abd18d922cce8fcd79a79deeeaa3
Federico Sandrelli <federico.sandrelli@gmail.com>
2017-06-11 14:27:25 +0200

    Fixed Json file + added EventEndMatch

---
+++ src/main/java/it/polimi/ingsw/gc_12/event/EventEndMatch.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.gc_12.event;
+
+import it.polimi.ingsw.gc_12.effect.EffectProvider;
+
+import java.util.List;
+
+/**
+ * Created by feder on 2017-06-11.
+ */
+public class EventEndMatch extends Event {
+    @Override
+    public List<Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public List<EffectProvider> getEffectProviders() {
+        return null;
+    }
+}
