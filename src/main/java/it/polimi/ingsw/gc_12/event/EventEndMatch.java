82cd91c4157c5a5bc93816d925eccc59234246e0
Federico Sandrelli <federico.sandrelli@gmail.com>
2017-06-14 15:42:57 +0200

    Added EventEndMatch

--- src/main/java/it/polimi/ingsw/gc_12/event/EventEndMatch.java
+++ src/main/java/it/polimi/ingsw/gc_12/event/EventEndMatch.java
@@ -1,20 +1,31 @@
 package it.polimi.ingsw.gc_12.event;
 
+import it.polimi.ingsw.gc_12.Player;
 import it.polimi.ingsw.gc_12.effect.EffectProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by feder on 2017-06-11.
- */
 public class EventEndMatch extends Event {
+
+
+    public EventEndMatch(Player player) {
+        super(player);
+
+        effectProviders.addAll(player.getCards());
+        effectProviders.add(player.getPersonalBoard().getResourceContainer());
+    }
+
+    public EventEndMatch(){
+    }
+
     @Override
     public List<Object> getAttributes() {
-        return null;
+        return new ArrayList<Object>();
     }
 
     @Override
     public List<EffectProvider> getEffectProviders() {
-        return null;
+        return effectProviders;
     }
 }
