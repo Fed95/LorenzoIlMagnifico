4bfc880fe20ff146e50e8c9ac2ba58bf0300a815
ruggero95 <10454587@polimi.it>
2017-06-18 13:08:22 +0200

    Added responsive

--- src/main/java/it/polimi/ingsw/gc_12/java_fx/MainBoard.java
+++ src/main/java/it/polimi/ingsw/gc_12/java_fx/MainBoard.java
@@ -2,9 +2,12 @@ package it.polimi.ingsw.gc_12.java_fx;
 
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 public class MainBoard extends Application{
@@ -17,12 +20,25 @@ public class MainBoard extends Application{
 	public void start(Stage primaryStage) throws Exception{
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(MainBoard.class.getResource("/FXML/FXMLMainBoard.fxml"));
-		Parent rootLayout = loader.load();
+		Pane rootLayout = loader.load();
 		//Parent rootLayout= FXMLLoader.load(getClass().getResource("/FXML/FXMLMainBoard.fxml"));
 		Scene scene = new Scene(rootLayout,1920,1080);
 		primaryStage.setTitle("Lorenzo il magnifico");
+		//double dim = 1000.0/1080.0;
+		//Scale scale = new Scale(1, dim);
+		//scale.setPivotX(0);
+		//scale.setPivotY(0);
+		//scene.getRoot().getTransforms().setAll(scale);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		responsive(scene, rootLayout, primaryStage);
 	}
-	
+	private void responsive(Scene scene, Pane root, Stage primaryStage){
+		final double initWidth  = scene.getWidth();
+		final double initHeight = scene.getHeight();
+		final double ratio      = initWidth / initHeight;
+		ResponsiveListener sizeListener = new ResponsiveListener(scene, ratio, initHeight, initWidth, root, primaryStage);
+		scene.widthProperty().addListener(sizeListener);
+		scene.heightProperty().addListener(sizeListener);
+	}
 }
---
+++ src/main/java/it/polimi/ingsw/gc_12/java_fx/ResponsiveListener.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.gc_12.java_fx;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
+
+
+/**
+ * Created by rugge on 17/06/2017.
+ */
+public class ResponsiveListener implements ChangeListener<Number> {
+    private final Scene scene;
+    private final double ratio;
+    private final double initHeight;
+    private final double initWidth;
+    private final Pane root;
+    private final Stage primaryStage;
+
+    public ResponsiveListener(Scene scene, double ratio, double initHeight, double initWidth, Pane root, Stage primaryStage) {
+        this.scene = scene;
+        this.ratio = ratio;
+        this.initHeight = initHeight;
+        this.initWidth = initWidth;
+        this.root = root;
+        this.primaryStage = primaryStage;
+    }
+    @Override
+    public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+        final double newWidth  = scene.getWidth();
+        final double newHeight = scene.getHeight();
+        System.out.println(newHeight);
+        System.out.println(newWidth);
+        double scaleFactor;
+        //usa come fattore di scala il lato piu piccolo  della finestra
+        if(newWidth/newHeight > ratio){
+            scaleFactor = newHeight / initHeight;
+        }else{
+            scaleFactor = newWidth / initWidth;
+        }
+
+        Scale scale = new Scale(scaleFactor, scaleFactor);
+        scale.setPivotX(0);
+        scale.setPivotY(0);
+        scene.getRoot().getTransforms().setAll(scale);
+    }
+}
