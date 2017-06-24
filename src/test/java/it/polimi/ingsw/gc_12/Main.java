7dd314e52cd6767bfe903264fd3fbdfcd5282fe8
Federico Sandrelli <federico.sandrelli@gmail.com>
2017-06-24 22:05:35 +0200

    Implemented Rounds + Excommunications

--- src/test/java/it/polimi/ingsw/gc_12/Main.java
+++ src/test/java/it/polimi/ingsw/gc_12/Main.java
@@ -9,8 +9,21 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+/*
+		List<Player> players = new ArrayList<>();
 
+		players.add(new Player("bob", PlayerColor.BLUE));
+		players.add(new Player("carl", PlayerColor.GREEN));
+		players.add(new Player("aaa", PlayerColor.RED));
+		players.add(new Player("vvv", PlayerColor.YELLOW));
 
+		Match match = new Match();
+		match.init(players);
+
+		SaverExcommunications saver = new SaverExcommunications(match);
+		saver.create();
+		*/
 	}
 
 }
+
