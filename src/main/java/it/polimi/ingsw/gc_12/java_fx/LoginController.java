4a3fba4c77862bb293b761d6581479cc086b3aa8
ruggero95 <10454587@polimi.it>
2017-06-28 17:36:29 +0200

    Added login form fxml and controller

---
+++ src/main/java/it/polimi/ingsw/gc_12/java_fx/LoginController.java
@@ -0,0 +1,75 @@
+package it.polimi.ingsw.gc_12.java_fx;
+
+import it.polimi.ingsw.gc_12.client.ClientFactory;
+import it.polimi.ingsw.gc_12.client.rmi.ClientRMI;
+import it.polimi.ingsw.gc_12.client.socket.ClientSocket;
+import it.polimi.ingsw.gc_12.mvc.View;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.util.ResourceBundle;
+
+
+/**
+ * Created by rugge on 28/06/2017.
+ */
+public class LoginController implements Initializable {
+    @FXML private TextField nameField;
+    @FXML private ChoiceBox connectionChoice;
+    @FXML private Button playButton;
+    @FXML private Pane mainPane;
+    private ObservableList<String> connectionList = FXCollections.observableArrayList("RMI","SOCKET");
+    private View mainBoard;
+
+    @FXML void startGame(MouseEvent event) throws NotBoundException, AlreadyBoundException, CloneNotSupportedException, IOException {
+        String name = nameField.getText();
+        String connection = connectionChoice.getValue().toString();
+        if(isNameValid(name)) {
+            if(connection.equals("RMI")){
+                ClientRMI clientRMI = new ClientRMI();
+                ClientFactory.setClientSender(clientRMI);
+                clientRMI.start(mainBoard, name);
+            }else if(connection.equals("SOCKET")) {
+                ClientSocket clientSocket = new ClientSocket();
+                clientSocket.startClient(mainBoard, name);
+            }
+            MainBoard mb = (MainBoard) mainBoard;
+            mb.changeScene("FXMLMainBoard", 1980, 1080, true, "Lorenzo il magnifico");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        connectionChoice.setValue("RMI");
+        connectionChoice.setItems(connectionList);
+    }
+    public void setMainBoard(MainBoard mainBoard){
+        this.mainBoard = mainBoard;
+    }
+    private Boolean isNameValid(String name){
+        if(name.equals("") || name.equals(" ")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Empty Name");
+            alert.setContentText("The Name is required for starting the game");
+            alert.showAndWait();
+            return false;
+        }
+        return true;
+    }
+}
