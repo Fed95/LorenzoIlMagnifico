fd653fcf642316e3a7b4c389517d0b40ae975389
Federico Sandrelli <federico.sandrelli@gmail.com>
2017-06-29 12:05:32 +0200

    Typos

--- src/main/java/it/polimi/ingsw/gc_12/personal_board/ResourcesContainer.java
+++ src/main/java/it/polimi/ingsw/gc_12/personal_board/ResourcesContainer.java
@@ -4,10 +4,7 @@ import it.polimi.ingsw.gc_12.effect.Effect;
 import it.polimi.ingsw.gc_12.effect.EffectChangeResource;
 import it.polimi.ingsw.gc_12.effect.EffectProvider;
 import it.polimi.ingsw.gc_12.event.EventEndMatch;
-import it.polimi.ingsw.gc_12.resource.Resource;
-import it.polimi.ingsw.gc_12.resource.ResourceExchange;
-import it.polimi.ingsw.gc_12.resource.ResourceType;
-import it.polimi.ingsw.gc_12.resource.VictoryPoint;
+import it.polimi.ingsw.gc_12.resource.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,8 +27,10 @@ public class ResourcesContainer implements EffectProvider {
     @Override
     public List<Effect> getEffects() {
         int value = 0;
-        for(Resource resource : resources.values())
+        for(Resource resource : resources.values()) {
+            if(resource instanceof Money || resource instanceof Wood || resource instanceof Stone)
             value += resource.getValue();
+        }
         ResourceExchange exchange = new ResourceExchange(null, new VictoryPoint(value/5));
         effects.add(new EffectChangeResource(new EventEndMatch(), exchange, false));
         return effects;
