3fccbf8bdc7b2bc2f801a34992700546475f3324
Marco Varrone <marco.varrone@mail.polimi.it>
2017-06-30 15:23:00 +0200

    Fix ConcurrentModificationException

--- src/main/java/it/polimi/ingsw/gc_12/server/observer/Observable.java
+++ src/main/java/it/polimi/ingsw/gc_12/server/observer/Observable.java
@@ -13,30 +13,32 @@ public abstract class Observable<C> {
 		this.observers=new ArrayList<Observer<C>>();
 	}
 	
-	public void registerObserver(Observer<C> o){
+	public synchronized void registerObserver(Observer<C> o){
 		this.observers.add(o);
 	}
 
-	public void unregisterObserver(Observer<C> o){
+	public synchronized void unregisterObserver(Observer<C> o){
 		this.observers.remove(o);
 	}
 
-	public void unregisterObserver(Class c) {
+	public synchronized void unregisterObserver(Class c) {
 		observers.removeIf(c::isInstance);
 	}
 	
-	public void notifyObserver(C c) {
+	public synchronized void notifyObserver(C c) {
 		System.out.println("I am the " + this.getClass().getSimpleName() + ". I have been notified about " + c.getClass());
-		ListIterator<Observer<C>> itr = this.observers.listIterator();
-		while(itr.hasNext()) {
-			Observer<C> o = itr.next();
+		List<Observer<C>> newObservers = new ArrayList<>();
+		newObservers.addAll(observers);
+		for(Observer<C> o: newObservers) {
 			o.update(c);
 		}
 	}
 	
-	protected void notifyObserver() {
-		for(Observer<C> o: this.observers){
+	protected synchronized void notifyObserver() {
+		List<Observer<C>> newObservers = new ArrayList<>();
+		newObservers.addAll(observers);
+		for(Observer<C> o: newObservers) {
 			o.update();
-		}		
+		}
 	}
 }
