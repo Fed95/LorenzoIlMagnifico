7f85546716137aa3734118bcdc179eff9bd491e9
Marco Varrone <marco.varrone@mail.polimi.it>
2017-07-01 11:08:50 +0200

    Fix match stucking on start

--- src/main/java/it/polimi/ingsw/gc_12/server/observer/Observable.java
+++ src/main/java/it/polimi/ingsw/gc_12/server/observer/Observable.java
@@ -13,19 +13,19 @@ public abstract class Observable<C> {
 		this.observers=new ArrayList<Observer<C>>();
 	}
 	
-	public synchronized void registerObserver(Observer<C> o){
+	public void registerObserver(Observer<C> o){
 		this.observers.add(o);
 	}
 
-	public synchronized void unregisterObserver(Observer<C> o){
+	public void unregisterObserver(Observer<C> o){
 		this.observers.remove(o);
 	}
 
-	public synchronized void unregisterObserver(Class c) {
+	public void unregisterObserver(Class c) {
 		observers.removeIf(c::isInstance);
 	}
 	
-	public synchronized void notifyObserver(C c) {
+	public void notifyObserver(C c) {
 		System.out.println("I am the " + this.getClass().getSimpleName() + ". I have been notified about " + c.getClass());
 		List<Observer<C>> newObservers = new ArrayList<>();
 		newObservers.addAll(observers);
@@ -34,7 +34,7 @@ public abstract class Observable<C> {
 		}
 	}
 	
-	public synchronized void notifyObserver() {
+	public void notifyObserver() {
 		List<Observer<C>> newObservers = new ArrayList<>();
 		newObservers.addAll(observers);
 		for(Observer<C> o: newObservers) {
