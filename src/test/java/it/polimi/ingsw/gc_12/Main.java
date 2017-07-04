55819fb9b67a7054461defa5c7eb6c8132af4e8d
Marco Varrone <marco.varrone@mail.polimi.it>
2017-07-04 16:01:03 +0200

    Compile fix

--- src/test/java/it/polimi/ingsw/gc_12/Main.java
+++ src/test/java/it/polimi/ingsw/gc_12/Main.java
@@ -1,12 +1,5 @@
 package it.polimi.ingsw.gc_12;
 
-import it.polimi.ingsw.gc_12.json.saver.SaverCard;
-import it.polimi.ingsw.gc_12.json.saver.SaverExcommunications;
-import it.polimi.ingsw.gc_12.json.saver.SaverTest;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
